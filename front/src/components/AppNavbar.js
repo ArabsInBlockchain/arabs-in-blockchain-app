d6e0fa0715292b36524efcc515989a8c8ead6331
emanherawy <eman.herawy@gmail.com>
2020-10-14 16:47:59 +0200

    update navbar

--- front/src/components/AppNavbar.js
+++ front/src/components/AppNavbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import {
-    MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem,  MDBNavbarToggler, MDBCollapse, MDBFormInline,
+    MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavbarToggler, MDBCollapse, MDBFormInline,
     MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
 } from "mdbreact";
 import logo from '../logo.svg'
@@ -16,134 +16,147 @@ class AppNavbar extends Component {
 
         this.state = { isOpen: false }
     }
+    componentDidMount() {
+        document.querySelector('nav').style.height = '65px';
+    }
 
+    componentWillUnmount() {
+        document.querySelector('nav').style.height = 'auto';
+    }
     toggle() {
         this.setState(prevState => ({ isOpen: !prevState.isOpen }))
     }
 
     render() {
+        const navStyle = { marginTop: '4rem' };
+
         return (
 
-            <MDBNavbar color="blue-gradient" className="font-weight-bold py-4 px-2 mb-4" dark expand="md">
-            <MDBNavbarBrand>
-                          <LocalizedLink to="/" className="navbar-brand">
-                              <img
-                                  src={logo}
-                                  width="30"
-                                  height="30"
-                                  className="d-inline-block align-top"
-                                  alt={t('app_name')}
-                              />
+            <MDBNavbar color="blue-gradient" className="font-weight-bold py-4 px-2 mb-4 justify-content-center" dark
 
-                          </LocalizedLink>
-                          <strong className="white-text">  {t('app_name')}</strong>
-                      </MDBNavbarBrand>
-              <MDBNavbarToggler onClick={this.toggle} />
-              <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-                <MDBNavbarNav left>
-                <MDBNavItem active>
-                    <LocalizedLink to="/home" className="nav-link">
-                                      {t('menu_home')}
+                expand='md'
+                fixed='top'
+                scrolling
+            >
+                <MDBNavbarBrand>
+                    <LocalizedLink to="/" className="navbar-brand">
+                        <img
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt={t('app_name')}
+                        />
+
                     </LocalizedLink>
-                 </MDBNavItem>
-                <MDBNavItem >
-                    <LocalizedLink to="/activity" className="nav-link">
-                                      {t('menu_activity')}
-                    </LocalizedLink>
-                 </MDBNavItem>
-                <MDBNavItem >
-                    <LocalizedLink to="/contribution" className="nav-link">
-                                      {t('menu_contribution')}
-                    </LocalizedLink>
-                 </MDBNavItem>
-                <MDBNavItem >
-                    <LocalizedLink to="/advisory" className="nav-link">
-                                      {t('menu_advisory')}
-                    </LocalizedLink>
-                 </MDBNavItem>
-                <MDBNavItem >
-                    <LocalizedLink to="/partner" className="nav-link">
-                                      {t('menu_partner')}
-                    </LocalizedLink>
-                 </MDBNavItem>
-                <MDBNavItem >
-                    <LocalizedLink to="/research" className="nav-link">
-                                      {t('menu_researchHub')}
-                    </LocalizedLink>
-                 </MDBNavItem>
+                    <strong className="white-text">  {t('app_name')}</strong>
+                </MDBNavbarBrand>
+                <MDBNavbarToggler onClick={this.toggle} />
+                <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+                    <MDBNavbarNav right>
+                        <MDBNavItem active>
+                            <MDBDropdown>
+                                <MDBDropdownToggle nav caret>
+                                    <div className="d-none d-md-inline">{t('menu_service')}</div>
+                                </MDBDropdownToggle>
+                                <MDBDropdownMenu className="dropdown-default">
+                                    <MDBDropdownItem href="#!">   <LocalizedLink to="/activity" className="navLink">
+                                        {t('menu_activity')}
+                                    </LocalizedLink></MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">   <LocalizedLink to="/contribution" className="navLink">
+                                        {t('menu_contribution')}
+                                    </LocalizedLink></MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">   <LocalizedLink to="/advisory" className="navLink">
+                                        {t('menu_advisory')}
+                                    </LocalizedLink></MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">       <LocalizedLink to="/research" className="navLink">
+                                        {t('menu_researchHub')}
+                                    </LocalizedLink></MDBDropdownItem>
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
+
+                        </MDBNavItem>
+                        {/* <MDBNavItem active>
+                            <LocalizedLink to="/home" className="navLink">
+                                {t('menu_home')}
+                            </LocalizedLink>
+                        </MDBNavItem> */}
 
 
+                        <MDBNavItem >
+                            <LocalizedLink to="/partner" className="nav-link">
+                                {t('menu_partner')}
+                            </LocalizedLink>
+                        </MDBNavItem>
+                        <MDBNavItem active>
+                            <MDBDropdown>
+                                <MDBDropdownToggle nav caret>
+                                    <div className="d-none d-md-inline">  {t('menu_about')}</div>
+                                </MDBDropdownToggle>
+                                <MDBDropdownMenu className="dropdown-default">
+                                    <MDBDropdownItem >     <LocalizedLink to="/about" className="navLink">
+                                        {t('menu_about')}
+                                    </LocalizedLink></MDBDropdownItem>
+                                    <MDBDropdownItem >        <LocalizedLink to="/join" className="navLink">
+                                        {t('menu_join')}
+                                    </LocalizedLink></MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">  <LocalizedLink to="/subscribe" className="navLink">
+                                        {t('menu_subscribe')}
+                                    </LocalizedLink></MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">      <LocalizedLink to="/contact" className="navLink">
+                                        {t('menu_contactUs')}
+                                    </LocalizedLink></MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">     <LocalizedLink to="/press" className="navLink">
+                                        {t('menu_press')}
+                                    </LocalizedLink></MDBDropdownItem>
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
 
-                </MDBNavbarNav>
-                <MDBNavbarNav right>
-                <MDBNavItem >
-                              <LocalizedLink to="/press" className="nav-link">
-                                      {t('menu_press')}
-                                  </LocalizedLink>
-                              </MDBNavItem>
-                              <MDBNavItem >
-                              <LocalizedLink to="/join" className="nav-link">
-                                      {t('menu_join')}
-                                  </LocalizedLink>
-                              </MDBNavItem>
-                              <MDBNavItem >
-                              <LocalizedLink to="/about" className="nav-link">
-                                      {t('menu_about')}
-                                  </LocalizedLink>
-                              </MDBNavItem>
-                              <MDBNavItem >
-                              <LocalizedLink to="/subscribe" className="nav-link">
-                                      {t('menu_subscribe')}
-                                  </LocalizedLink>
-                              </MDBNavItem>
-                              <MDBNavItem >
-                              <LocalizedLink to="/contact" className="nav-link">
-                                      {t('menu_contactUs')}
-                                  </LocalizedLink>
-                              </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBFormInline waves>
-                      <div className="md-form my-0">
-                        <input className="form-control mr-sm-2" type="text" placeholder=   {t('menu_search-placeholder')} aria-label=   {t('menu_search')} />
-                      </div>
-                    </MDBFormInline>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                                  <MDBDropdown>
-                                      <MDBDropdownToggle nav caret>
+                        </MDBNavItem>
 
-                                          <span
-                                              role="img"
-                                              aria-label="globe"
-                                              className="globe-icon"
-                                          >
-                                              🌐
+                        <MDBNavItem>
+                            <MDBFormInline waves>
+                                <div className="md-form my-0">
+                                    <input className="form-control mr-sm-2" type="text" placeholder={t('menu_search-placeholder')} aria-label={t('menu_search')} />
+                                </div>
+                            </MDBFormInline>
+                        </MDBNavItem>
+                        <MDBNavItem>
+                            <MDBDropdown>
+                                <MDBDropdownToggle nav caret>
+
+                                    <span
+                                        role="img"
+                                        aria-label="globe"
+                                        className="globe-icon"
+                                    >
+                                        🌐
                                       </span>
 
-                                          {/* {t('language')} */}
-                                                          </MDBDropdownToggle>
-                                      <MDBDropdownMenu>
-                                          {locales.map(locale => (
-                                              <MDBDropdownItem key={locale.code}>
+                                    {/* {t('language')} */}
+                                </MDBDropdownToggle>
+                                <MDBDropdownMenu>
+                                    {locales.map(locale => (
+                                        <MDBDropdownItem key={locale.code}>
 
-                                                  <Link
+                                            <Link
 
-                                                      to={`/${locale.code}`}
-                                                      className="dropdown-item"
-                                                  >
-                                                      {locale.name}
-                                                  </Link>
-                                              </MDBDropdownItem>
-                                          ))}
-                                      </MDBDropdownMenu>
-                                  </MDBDropdown>
-                              </MDBNavItem>
-                </MDBNavbarNav>
-              </MDBCollapse>
+                                                to={`/${locale.code}`}
+                                                className="dropdown-item"
+                                            >
+                                                {locale.name}
+                                            </Link>
+                                        </MDBDropdownItem>
+                                    ))}
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
+                        </MDBNavItem>
+                    </MDBNavbarNav>
+                </MDBCollapse>
             </MDBNavbar>
 
-          );
-        }
+        );
+    }
 }
 
 export default AppNavbar
